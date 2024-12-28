39fdf829bb7ec6e210af59304f76126b4ae367df
orik01556340144 <121636853+orik01556340144@users.noreply.github.com>
2024-12-28 20:41:21 +0600

    added contact and about

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
-
-
+import'./About.css'
 
 const About= () => {
   return (
     <section className="about-us-page">
       <h1>About Us</h1>
-      
+      <div className="about-section mission">
+        <h2>Our Mission</h2>
+        <p>
+          At Purrfect Adoption, our mission is to provide loving homes for cats in need
+          while educating the community about responsible pet ownership.
+        </p>
+      </div>
+      <div className="about-section history">
+        <h2>Our History</h2>
+        <p>
+         I ,Farhan Labib created it in my dreams.
+        </p>
+      </div>
+      <div className="about-section team">
+        <h2>Meet Our Team</h2>
+       
+        <div className="team-members">
+          <div className="team-member">
+            <img src="src\assets\img1.webp" alt="Team Member 1" className="team-image" />
+            <p className="team-name">Dr. Ken kaneki</p>
+          </div>
+          <div className="team-member">
+            <img src="src\assets\download (11).jpeg" alt="Team Member 2" className="team-image" />
+            <p className="team-name">Mr.MonkeyD luffy</p>
+          </div>
+          <div className="team-member">
+            <img src="src\assets\images.webp" alt="Team Member 3" className="team-image" />
+            <p className="team-name">Dr. uzumaki narotu</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-
+import './Contact.css'
 
 const Contact = () => {
   return (
     <section className="contact-page">
       <h2>Contact Us</h2>
-     
+      <p>
+        We’d love to hear from you! Please use the form below to reach out to us with any
+        questions, comments, or concerns.
+      </p>
+      <form className="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" placeholder="Enter your name" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" id="phone" placeholder="Enter your number" />
+        </div>
+        
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        
+        <button type="submit">Submit</button>
+      </form>
     </section>
   );
 };
 
 export default Contact;
+
